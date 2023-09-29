3a7a28b4f14417ec916ee70f92136459b1085386
chawn <chawput@gmail.com>
2023-09-29 14:48:00 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<div className='min-h-screen flex justify-center items-center'>
+			<h1 className='text-3xl font-bold text-blue-600'>
+				Installed :: Vite + React + Typescript + Tailwind CSS 3
+			</h1>
+		</div>
+	);
+}
